d32a310329349e415bed01c637b08100b5065ad4
HiyokoTarot <rockerdeys@gmail.com>
2026-06-02 12:09:44 +0900

    Add files via upload

--- eki2.js
+++ eki2.js
@@ -1,15 +1,26 @@
 function drawLine() {
-  const r = Math.floor(Math.random() * 16) + 1; // 1〜16
+  const r = Math.floor(Math.random() * 16) + 1;
 
-  if (r === 1) return 6;          // 1/16
-  if (r <= 6) return 7;           // 5/16
-  if (r <= 13) return 8;          // 7/16
-  return 9;                       // 3/16
+  if (r === 1) return 6;
+  if (r <= 6) return 7;
+  if (r <= 13) return 8;
+  return 9;
+}
+
+function lineType(n) {
+  if (n === 6) return "老陰";
+  if (n === 7) return "少陽";
+  if (n === 8) return "少陰";
+  if (n === 9) return "老陽";
 }
 
 function lineSymbol(n) {
   if (n === 6 || n === 8) return "-- --";
   if (n === 7 || n === 9) return "-----";
+}
+
+function isChanging(n) {
+  return n === 6 || n === 9;
 }
 
 function drawHexagram() {
@@ -25,23 +36,5 @@ function drawHexagram() {
     result += lineSymbol(lines[i]) + "\n";
   }
 
-
-  document.getElementById("result").textContent =
-    lines.join(", ");
-}
-
-function lineType(n) {
-  if (n === 6) return "老陰";
-  if (n === 7) return "少陽";
-  if (n === 8) return "少陰";
-  if (n === 9) return "老陽";
-}
-
-function lineSymbol(n) {
-  if (n === 6 || n === 8) return "-- --"; // 陰
-  if (n === 7 || n === 9) return "-----"; // 陽
-}
-
-function isChanging(n) {
-  return n === 6 || n === 9;
+  document.getElementById("result").textContent = result;
 }
