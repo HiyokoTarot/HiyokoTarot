8b867f70fb36d838a9a514650f39180680bbcc3a
HiyokoTarot <rockerdeys@gmail.com>
2026-06-16 09:11:28 +0900

    Add files via upload

--- logic/eki.js
+++ logic/eki.js
@@ -7,42 +7,9 @@ function drawLine() {
   return 9;
 }
 
-function lineType(n) {
-  if (n === 6) return "老陰";
-  if (n === 7) return "少陽";
-  if (n === 8) return "少陰";
-  if (n === 9) return "老陽";
-}
-
-function lineSymbol(n) {
-  if (n === 6 || n === 8) return "-- --";
-  if (n === 7 || n === 9) return "-----";
-}
-
 function isChanging(n) {
   return n === 6 || n === 9;
 }
-
-function drawHexagram() {
-  const lines = [];
-
-  for(let i = 0; i < 6; i++) {
-    lines.push(drawLine());
-  }
-
-  let result = "";
-
-for(let i = 5; i >= 0; i--) {
-  result += lineSymbol(lines[i])
-         + " "
-         + lineType(lines[i])
-         + "\n";  }
-
-return {
-  lines,
-  changedLines,
-  changedHexagram
-};
 
 function getChangedLines(lines) {
   const names = ["初爻", "二爻", "三爻", "四爻", "五爻", "上爻"];
